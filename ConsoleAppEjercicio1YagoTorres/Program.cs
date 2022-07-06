fe8d05430dba50398ce4745191f0733e617cabaa
YagoT1 <yagotorres075@gmail.com>
2022-07-05 22:25:04 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppEjercicio1YagoTorres/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppEjercicio1YagoTorres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese la velocidad del automóvil");
+            var Velocidad = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tiempo del recorrido en minutos");
+            var Tiempo = Double.Parse(Console.ReadLine());
+            double Distancia = Velocidad * Tiempo;
+            Distancia = Distancia * Tiempo;
+            Console.WriteLine($"el automóvil recorrió una distancia de: {Distancia} km");
+            Console.ReadLine();
+        }
+    }
+}
